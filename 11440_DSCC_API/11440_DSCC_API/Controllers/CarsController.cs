0f43283b133ef7bc1e41f85ac0660c93affab621
Saidabbos Sadikov <00011440@wiut.uz>
2023-11-07 18:15:18 +0500

    Created API, need to cofigure connection string and add migrations

---
+++ 11440_DSCC_API/11440_DSCC_API/Controllers/CarsController.cs
@@ -0,0 +1,62 @@
+﻿using _11440_DSCC_API.Data;
+using _11440_DSCC_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _11440_DSCC_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        public CarsDbContext CarsDbContext { get; set; }
+
+        public CarsController(CarsDbContext dbContext) 
+        {
+            CarsDbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Car>> GetCars()
+        {
+            return CarsDbContext.Cars.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Car> GetCar(int id)
+        {
+            return CarsDbContext.Cars.Find(id);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult EditCar(int id, Car car)
+        {
+            Car oldCar = CarsDbContext.Cars.Find(id);
+            CarsDbContext.Entry(oldCar).State = EntityState.Modified;
+            oldCar.Model = car.Model;
+            oldCar.Manufacturer = car.Manufacturer;
+            oldCar.Price = car.Price;
+
+            CarsDbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCar(Car car)
+        {
+            CarsDbContext.Cars.Add(car);
+            CarsDbContext.SaveChanges();
+            return CreatedAtAction("GetCar", new { id = car.Id }, car);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCar(int id)
+        {
+            CarsDbContext.Cars.Remove(CarsDbContext.Cars.Find(id));
+            CarsDbContext.SaveChanges();
+            return NoContent();
+        }
+    }
+}
--- 11440_DSCC_API/11440_DSCC_API/Program.cs
+++ 11440_DSCC_API/11440_DSCC_API/Program.cs
@@ -1,3 +1,6 @@
+using _11440_DSCC_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CarsDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("CarsDb")));
 
 var app = builder.Build();
 
